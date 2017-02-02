39972a080cc9d1a5f253841894b5de36b3e419d2
danielagara <daniela_mariel_garat@hotmail.com>
2017-02-02 20:23:03 -0300

    Primer commit
    Hicimos el ejercicio 1 y 2 en clase.

--- 1-EntradaSalida/jsEntradaSalida-1.js
+++ 1-EntradaSalida/jsEntradaSalida-1.js
@@ -1,6 +1,14 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostar()
 {
-	
+	alert("Esto funciona de maravilla");
+	//Esto es un comentario de una linea
+	/*
+	http://www.w3schools.com/
+	fuente de datos
+
+	*/
+
+	//alert para mostrar palabra con "comillas dobles"
 }
 
--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,22 @@
 y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
 function Mostar()
 {
-	
+	//var nombre = "Daniela";
+	nombre = prompt("Ingresa tu nombre");
+	alert(nombre);
+
+
+
+
+
+
+
+
+
+
+	//ejemplo: var person = prompt("Ingresa tu nombre", "Natalia Natalia");
+	//var: variable, sin espacios, ej: NombreDelUsuario
+	//prompt: siempre se debe usar con un igual antes y aclarando a que dato se le asigna
+	//f12 p/ver codigo de pagina
 }
 
