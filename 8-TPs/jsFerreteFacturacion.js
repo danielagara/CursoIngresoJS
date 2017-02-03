2e6fc95b599626b6f9aed4dbdddb9befd2ce02a2
danielagara <daniela_mariel_garat@hotmail.com>
2017-02-03 20:28:23 -0300

    tp hecho
    Funciona

--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -5,13 +5,48 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
-	
+	var precioUno;
+	var precioDos;
+	var precioTres;
+	precioUno=document.getElementById('PrecioUno').value;
+	precioDos=document.getElementById('PrecioDos').value;
+	precioTres=document.getElementById('PrecioTres').value;
+	precioUno=parseInt(precioUno);
+	precioDos=parseInt(precioDos);
+	precioTres=parseInt(precioTres);
+	var resultado=(precioUno+precioDos+precioTres);
+	alert(resultado);
+
 }
 function Promedio () 
 {
-	
+	var precioUno;
+	var precioDos;
+	var precioTres;
+	precioUno=document.getElementById('PrecioUno').value;
+	precioDos=document.getElementById('PrecioDos').value;
+	precioTres=document.getElementById('PrecioTres').value;
+	precioUno=parseInt(precioUno);
+	precioDos=parseInt(precioDos);
+	precioTres=parseInt(precioTres);
+	resultado=(precioUno+precioDos+precioTres)/3;
+	alert(resultado);
 }
 function PrecioFinal () 
 {
-	
+	var precioUno;
+	var precioDos;
+	var precioTres;
+	precioUno=document.getElementById('PrecioUno').value;
+	precioDos=document.getElementById('PrecioDos').value;
+	precioTres=document.getElementById('PrecioTres').value;
+	precioUno=parseInt(precioUno);
+	precioDos=parseInt(precioDos);
+	precioTres=parseInt(precioTres);
+	var preciofinal=(precioUno+precioDos+precioTres);
+	var iva=(preciofinal*21)/100;
+	//var resultado=(preciofinal*2,1); NO ES 
+	var resultado=(preciofinal+iva);
+	alert(resultado);
+	//funciona, hacer cuenta y que este sin la operacion 
 }
