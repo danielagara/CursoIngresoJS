862d399b124a2123bfb82658bdbd55604ced5965
danielagara <daniela_mariel_garat@hotmail.com>
2017-02-02 22:08:42 -0300

    Primer commit
    Ejercicios de 3 a 6

---
+++ jsEntradaSalida-4.js
@@ -0,0 +1,14 @@
+/* 
+	Debemos lograr tomar un dato por 'PROMPT' 
+	y lo muestro por 'getElementById' al presionar el botón 'MOSTRAR'
+*/
+function Mostar()
+{
+	var nombre;
+	nombre=prompt("Ingrese su nombre");
+	document.getElementById('elNombre').value=nombre;
+	//al poner =nombre despues de value me devuelve lo que yo complete antes, en lugar de usarlo como variable.
+
+
+}
+
