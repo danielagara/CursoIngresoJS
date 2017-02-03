d89b4bcba07c716d27f0f58e691fd815ea50f84a
danielagara <daniela_mariel_garat@hotmail.com>
2017-02-03 19:32:19 -0300

    Ejercicios 9 y 10
    Funcionan y tienen explicacion de funciones abajo

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,19 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function MostrarAumento()
 {
-	
+	var importe;
+	var resultado;
+	var descuento;
+	importe=document.getElementById('importe').value;
+	importe=parseInt(importe);
+
+	descuento=(importe*10)/100;
+	resultado=(importe-descuento);
+
+	document.getElementById('resultado').value=resultado;
+
+
+/*declarar variables, tomar el id del primero que es el que ingresa el usuario, cambiar a parseint, declarar variable descuento
+con operacion, declarar variable recultado juntando las anteriores en este caso restando una con la otra, por ultimo declarar
+que el id tiene que tener la informacion de la variable resultado*/
 }
--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -7,12 +7,24 @@ function MostrarAumento()
 
 	var sueldo;
 	var resultado;
+	var aumento;
+	
 	sueldo=document.getElementById('sueldo').value;
-	resultado=document.getElementById('resultado').value;
 	sueldo=parseInt(sueldo);
-	resultado=parseInt(resultado);
-	var aumento=((sueldo*10)/100);
-	alert(resultado++aumento);
+	
+	aumento=(sueldo*10)/100;
+	
+	resultado=sueldo+aumento;
+
+
+	document.getElementById('resultado').value=resultado;
+
+	/*resultado a la derecha para que tome lo anterior
+	declarar variables, tomar el id del primero que es el que ingresa el usuario, cambiar a entero con parseint, declarar 
+	variable descuento con operacion que calcule el porcentaje, declarar variable resultado juntando las anteriores en este 
+	caso restando una con la otra, por ultimo declarar que el id tiene que tener la informacion de la variable resultado*/
+	
+
 	//console.log(resultado+aumento);
-	//NO FUNCIONA
+
 }
