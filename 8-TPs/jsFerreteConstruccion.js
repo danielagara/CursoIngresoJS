cdf74c280c4d91655734608c8f770253cf20330b
danielagara <daniela_mariel_garat@hotmail.com>
2017-02-06 10:02:35 -0300

    Tp hecho

--- 8-TPs/jsFerreteConstruccion.js
+++ 8-TPs/jsFerreteConstruccion.js
@@ -3,7 +3,7 @@ A.	 Mostrar la cantidad de alambre a comprar  si se ingresara el largo y el anch
 B.	Mostrar la cantidad de alambre a comprar  si se ingresara el radio  de un terreno circular y se debe alambra con tres hilos de alambre.
 C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de cal, debemos mostrar cuantas bolsas se necesitan de cada uno para las medidas que nos ingresen.
 */
-//TERMINAR
+
 function Rectangulo () 
 {
 	var largo;
@@ -11,36 +11,31 @@ function Rectangulo ()
 	var radio;
 	largo=document.getElementById('Largo').value;
 	ancho=document.getElementById('Ancho').value;
-	radio=document.getElementById('Radio').value;
 	largo=parseInt(largo);
 	ancho=parseInt(ancho);
-	radio=parseInt(radio);
-	var rectangulo=
+	var rectangulo=(largo*ancho)*3;
+	alert(rectangulo);
 
-//TERMINAR
 }
 function Circulo () 
 {
 	var largo;
-	var ancho;
-	var radio;
-	largo=document.getElementById('Largo').value;
-	ancho=document.getElementById('Ancho').value;
 	radio=document.getElementById('Radio').value;
-	largo=parseInt(largo);
-	ancho=parseInt(ancho);
 	radio=parseInt(radio);
+	var circulo=(3.14*radio)*3;
+	alert(circulo);
+
 }
 function Materiales () 
 {
 	var largo;
 	var ancho;
-	var radio;
 	largo=document.getElementById('Largo').value;
 	ancho=document.getElementById('Ancho').value;
-	radio=document.getElementById('Radio').value;
 	largo=parseInt(largo);
 	ancho=parseInt(ancho);
-	radio=parseInt(radio);
-//TERMINAR
+	var cemento=(largo*ancho)*2;
+	var cal=(largo*ancho)*3;
+	alert("Se necesitan "+cemento+" bolsas de cemento y "+cal+" bolsas de cal");
+
 }
