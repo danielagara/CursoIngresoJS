2155c5768e293fac15c0b38259d28dc4a02fef07
danielagara <daniela_mariel_garat@hotmail.com>
2017-02-06 11:09:36 -0300

    a terminar

--- 8-TPs/jsFerreteIluminacion.js
+++ 8-TPs/jsFerreteIluminacion.js
@@ -10,5 +10,12 @@ E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de 
  */
 function CalcularPrecio () 
 {
- 	
+ 	var cantidad;
+ 	cantidad=document.getElementById('Cantidad').value;
+ 	cantidad=parseInt(cantidad);
+
+ 	//TERMINAR (va con if?)
+
+ 	var precioDescuento;
+ 	document.getElementById('precioDescuento').value=precioDescuento;
 }
