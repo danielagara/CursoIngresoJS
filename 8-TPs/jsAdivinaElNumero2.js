ee5b7549a3c3423a70d6fe5a8f78318eca763582
danielagara <daniela_mariel_garat@hotmail.com>
2017-02-10 17:19:46 -0300

    terminados

--- 8-TPs/jsAdivinaElNumero2.js
+++ 8-TPs/jsAdivinaElNumero2.js
@@ -10,19 +10,57 @@ Desde  6 Intentos hasta 10:”falta técnica”
 Más de 10 intentos: “afortunado en el amor!!”.*/
 
 var numeroSecreto; 
+var numeroAzar;
 var contadorIntentos;
-
+var intentos = 0;
 function comenzar()
 {
 	//Genero el número RANDOM entre 1 y 100
-	 
+	numeroAzar=Math.floor((Math.random() * 100) + 1);
+	alert(numeroAzar);
 	
 
 }
 
 function verificar()
 {
+	numeroSecreto=document.getElementById('numero').value;
+	intentos++;
 	
-	
-
+	if(numeroSecreto==numeroAzar && intentos==1)
+	{
+		alert("Usted es un psiquico");
+		intentos = 0;
+	}
+	if(numeroSecreto==numeroAzar && intentos==2)
+	{
+		alert("Excelente percepción");
+		intentos = 0;
+	}
+	if(numeroSecreto==numeroAzar && intentos==3)
+	{
+		alert("Esto es suerte");
+		intentos = 0;
+	}
+	if(numeroSecreto==numeroAzar && intentos==4)
+	{
+		alert("Excelente técnica");
+		intentos = 0;
+	}
+	if(numeroSecreto==numeroAzar && intentos==5)
+	{
+		alert("Usted esta en la media");
+		intentos = 0;
+	}
+		//PREGUNTAR PQ ME TIRA UNO DE LOS OTROS MSJ CUANDO QUIERO HACER DE 6 A 10
+	if(numeroSecreto==numeroAzar && intentos>=6)
+	{
+		alert("Usted necesita práctica");
+		intentos = 0;
+	}
+	else if(numeroSecreto==numeroAzar && intentos>=10)
+	{
+		alert("Suertudo en el amor!!");
+		intentos= 0;
+	}
 }
