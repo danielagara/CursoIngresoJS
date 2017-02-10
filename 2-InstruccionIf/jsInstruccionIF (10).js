0fcee5538cf638b879752e097df088fe7ae1df1c
danielagara <daniela_mariel_garat@hotmail.com>
2017-02-10 15:56:34 -0300

    terminado

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,18 @@
 function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
-
+	var nota=Math.floor((Math.random() * 10) + 1);
+	if(nota>=9)
+	{
+		alert("EXCELENTE");
+	}
+	else if(nota>=4)
+	{
+		alert("APROBÓ");
+	}
+	else
+	{
+		alert("DESAPROBÓ");
+	}
+//ponele que funciona, revisar
 }//FIN DE LA FUNCIÓN
