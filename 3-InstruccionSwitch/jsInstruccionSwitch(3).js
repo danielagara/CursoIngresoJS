a0c932277591f81d2dbe0bec96e1e74dd9b6d6d8
danielagara <daniela_mariel_garat@hotmail.com>
2017-02-14 17:05:00 -0300

    terminados

--- 3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -4,7 +4,17 @@ function Mostrar()
 var mesDelAño = document.getElementById('mes').value;
 
 //alert (mesDelAño);
-	
+	switch(mesDelAño)
+	{
+		case "Febrero":
+		alert('Este mes no tiene mas de 29 días');
+		break;
+		//solo voy a dejar un caso especifico para un mes
+		default:
+		//y usando default directamente les plancho a todos los otros meses el mismo alert, else pero de switch
+		alert('Este mes tiene mas de 29 días');
+
+	}
 	
 
 
