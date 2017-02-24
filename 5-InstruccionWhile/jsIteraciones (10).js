e3465f277f12f4aaa1413f1c7af7b8bea02dcb3a
danielagara <daniela_mariel_garat@hotmail.com>
2017-02-23 22:05:11 -0300

    terminado

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -7,6 +7,14 @@ function Mostrar()
 	var numeroNegativo;
 	var acumuladorNegativos=0;
 	var acumuladorPositivos=0;
+	var contadorPositivos=0;
+	var contadorNegativos=0;
+	var contadorCeros=0;
+	var contadorPares=0;
+	var promedioPositivos;
+	var promedioNegativos;
+	var diferenciaPositivosNegativos;
+
 	//declarar contadores y variables 
 	
 	var respuesta="si";
@@ -21,6 +29,8 @@ function Mostrar()
 		{
 			numeroNegativo=numero;
 			acumuladorNegativos=acumuladorNegativos+numeroNegativo;
+			//acumuladorNegativos=parseInt(acumuladorNegativos);
+			contadorNegativos++;
 		}
 		else
 		{
@@ -28,15 +38,37 @@ function Mostrar()
 			{
 				numeroPositivo=numero;
 				acumuladorPositivos=acumuladorPositivos+numeroPositivo;
+				//acumuladorPositivos+=numeroPositivo; MANERA CORTA DE PONER UN ACUMULADOR
+				//acumuladorPositivos=parseInt(acumuladorPositivos);
+				contadorPositivos++;
 			}
-			//if
+			if(numero==0)
+			{
+				contadorCeros++;
+			}
+		}
+
+		if(numero % 2 == 0)
+		{
+			contadorPares++;
 		}
 
 	respuesta=prompt('Ingrese si para continuar');
+
 	}
 
-	document.writeln('la suma de los positivos es '+acumuladorPositivos);
-	document.writeln('la suma de los negativos es '+acumuladorNegativos);
+	promedioPositivos=(acumuladorPositivos/contadorPositivos);
+	promedioNegativos=(acumuladorNegativos/contadorNegativos);
+	diferenciaPositivosNegativos=(acumuladorPositivos-acumuladorNegativos);
+
+	document.writeln('La suma de los positivos es '+acumuladorPositivos+"<br>");
+	document.writeln('La suma de los negativos es '+acumuladorNegativos+"<br>");
+	document.writeln('Cantidad de numeros positivos '+contadorPositivos+"<br>");
+	document.writeln('Cantidad de numeros negativos '+contadorNegativos+"<br>");
+	document.writeln('Cantidad de ceros '+contadorCeros+"<br>");
+	document.writeln('Cantidad de numeros pares '+contadorPares+"<br>");
+	document.writeln('Promedio de numeros positivos '+promedioPositivos+"<br>");
+	document.writeln('Promedio de numeros negativos '+promedioNegativos+"<br>");
 
 
 }//FIN DE LA FUNCIÓN
